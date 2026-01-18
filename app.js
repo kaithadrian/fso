cb3da4a091f6053c22ab84e336675baa38b0f4c4
kaithadrian <kaithadrian@gmail.com>
2026-01-18 15:35:07 +0800

    add app.js, index.html

---
+++ app.js
@@ -0,0 +1,35 @@
+let username="assmole";
+const password="*******";
+
+let age;
+age = 2 + "3"
+console.log(age)
+
+
+let isloggedin = false;
+
+
+console.log(age>10)
+
+let a = 4;
+let b = 3;
+
+console.log(a ** b)
+console.log(a % b)
+
+a+=1
+a-=1
+a*=1
+a/=1
+a%=1
+console.log(a)
+
+
+
+username="Tom";
+console.log(username);
+username="Harry";
+console.log(username);
+console.log(1+"2"+3+10)
+document.body.style.background = "red";
+alert("Hello")
