852ed1c42499f605f790a72eff9e5a903514e6f1
kaithadrian <kaithadrian@gmail.com>
2026-01-18 17:02:08 +0800

    last commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+document.body.innerHTML = "<h1> Hello header </h>";
+document.body.innerHTML = document.body.innerHTML + "<button> click me </button>";
+document.body.style.background = "red";
+document.body.style.textAlign = "center"
+document.body.style.color = "blue";
+
+
 let username="assmole";
 const password="*******";
 
@@ -22,6 +29,8 @@ a-=1
 a*=1
 a/=1
 a%=1
+a++
+a--
 console.log(a)
 
 
@@ -31,5 +40,14 @@ console.log(username);
 username="Harry";
 console.log(username);
 console.log(1+"2"+3+10)
-document.body.style.background = "red";
+
 alert("Hello")
+
+
+
+
+function sayHello(name){
+    console.log("hello "+name)
+
+}
+
